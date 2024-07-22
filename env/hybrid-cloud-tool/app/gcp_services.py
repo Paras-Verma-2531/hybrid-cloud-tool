1f213b32c7fd623313d3020f1a0316915758279d
Parth Rawat <parthpriyanshu10@gmail.com>
2024-07-22 23:36:16 +0530

    gone

--- env/hybrid-cloud-tool/app/gcp_services.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import compute_v1
-
-def get_gcp_resources():
-    client = compute_v1.InstancesClient()
-    project = 'your-project-id'
-    zone = 'us-central1-a'
-    instances = client.list(project=project, zone=zone)
-    return instances
